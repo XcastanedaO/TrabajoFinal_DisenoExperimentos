0cf89f8c71ea114a8df024f2d619e06cbfe38d05
vseguro <79714681+vseguro@users.noreply.github.com>
2022-11-14 11:38:53 -0500

    Cambios

--- Registro de información.R
+++ Registro de información.R
@@ -17,7 +17,7 @@ vero_varta_in <- ((1.5+1.6)/2 + (1.4+1.5+1.6)/3 + (1.4+1.5+1.6)/3)/3
 vero_varta_fin <- ((0.9 + 1.0)/2 + (0.9 + 1.0)/2 + (0.9 + 1.0)/2)/3
 vero_varta_dif <- vero_varta_in - vero_varta_fin
 
-vero_duracell_in <- (1.1 + 1.1 + 1.1)/3
+vero_duracell_in <- ((1.5 + 1.6)/2 + 1.6 + 1.5)/3
 vero_duracell_fin <- (0.9 + (0.8 + 0.9)/2 + (0.8 + 0.9)/2)/3
 vero_duracell_dif <- vero_duracell_in - vero_duracell_fin
 
@@ -80,7 +80,7 @@ Xim_varta_in <- (1.6+(1.5 + 1.6)/2+1.6)/3
 Xim_varta_fin <- (0.9 + 0.9 + 0.9 )/3
 Xim_varta_dif <-  Xim_varta_in - Xim_varta_fin
 
-Xim_duracell_in <- ((1.5 + 1.6)/2 + (1.4 + 1.5 + 1.6)/2 + 1.6)/3
+Xim_duracell_in <- ((1.5 + 1.6)/2 + (1.4 + 1.5 + 1.6)/3 + 1.6)/3
 Xim_duracell_fin <- (0.9 + 0.9 + 0.9)/3
 Xim_duracell_dif <- Xim_duracell_in - Xim_duracell_fin
 
@@ -102,16 +102,6 @@ operador <- as.factor(c(rep( c("Veronica","Valentina", "Yojan", "Ximena"), 5)))
 
 voltaje <- data.frame(y_vol,marca,operador)
 
-modelo <- aov(y_vol ~ marca + operador, data = voltaje)
-summary(modelo)
-
-orden_vol <- c(vale_tronex_dif, Xim_duracell_dif, vero_futura_dif, Yojan_futura_dif,
-              vale_varta_dif, Xim_panasonic_dif, vero_duracell_dif, Yojan_varta_dif,
-              Xim_futura_dif, vale_futura_dif, vero_tronex_dif, Yojan_tronex_dif,
-              vero_varta_dif, Yojan_panasonic_dif,vale_panasonic_dif, Xim_tronex_dif,
-              vero_panasonic_dif,Yojan_duracell_dif,vale_duracell_dif,Xim_varta_dif
-)
-
 marca_ord <- as.factor(c("Tronex","Duracell","Futura","Futura", "Varta", "Panasonic", "Duracell", "Varta",
                          "Futura","Futura","Tronex","Tronex","Varta","Panasonic","Panasonic","Tronex",
                          "Panasonic","Duracell","Duracell","Varta"
@@ -122,95 +112,15 @@ operador_ord <- as.factor(c("Valentina","Ximena","Veronica","Yojan",
                             "Ximena", "Valentina", "Veronica","Yojan",
                             "Veronica","Yojan", "Valentina","Ximena",
                             "Veronica","Yojan", "Valentina","Ximena"))
+
+orden_vol <- c(vale_tronex_dif, Xim_duracell_dif, vero_futura_dif, Yojan_futura_dif,
+               vale_varta_dif, Xim_panasonic_dif, vero_duracell_dif, Yojan_varta_dif,
+               Xim_futura_dif, vale_futura_dif, vero_tronex_dif, Yojan_tronex_dif,
+               vero_varta_dif, Yojan_panasonic_dif,vale_panasonic_dif, Xim_tronex_dif,
+               vero_panasonic_dif,Yojan_duracell_dif,vale_duracell_dif,Xim_varta_dif
+)
+
 voltaje_ord <- data.frame(orden_vol,marca_ord,operador_ord)
-
-modelo_ord <- aov(orden_vol~marca_ord+operador_ord, data = voltaje_ord)
-summary(modelo_ord)
-
-#Homogeneidad
-bartlett.test(y_vol~marca)
-
-#Prueba de independencia
-library(car)
-#durbinWatsonTest(modelo)
-durbinWatsonTest(modelo_ord)
-
-#Gráfico de residuales vs orden de recolección
-plot(residuals(modelo_ord), pch =16, ylab="Residuales", xlab="Orden",
-     main="Gráfico de Orden vs Residuales")
-abline(h=0)
-
-qqnorm(residuals(modelo_ord))
-qqline(residuals(modelo_ord))
-
-shapiro.test(residuals(modelo_ord))
-
-#Modelo de un factor
-mol_marca <- aov(orden_vol~marca_ord)
-summary(mol_marca)
-
-library(asbio)
-tukey.add.test(orden_vol, marca_ord, operador_ord)
-
-#Modelo de interacción
-mol_int <- aov(orden_vol~marca_ord*operador_ord, data = voltaje_ord)
-summary(mol_int)
-# mol_int <- aov(y_vol~marca*operador, data = voltaje)
-# summary(mol_int)
-
-
-############################### Luminosidad ########################
-######### Luz #########
-y_luz <- c(28, 10, 54, 96,
-           28, 31, 44, 33,
-           6, 6, 7, 14,
-           41, 44, 51, 7,
-           26, 31, 45, 11)
-
-marca_luz <-c("Duracell", "Tronex", "Futura","Futura",
-              "Varta","Varta", "Duracell", "Panasonic",
-              "Tronex", "Tronex", "Futura", "Futura",
-              "Varta", "Panasonic", "Panasonic", "Tronex",
-              "Panasonic", "Duracell", "Duracell", "Varta")
-
-operador_luz <- c("Ximena", "Valentina", "Veronica", "Yojan",
-                  "Valentina", "Yojan", "Veronica", "Ximena",
-                  "Veronica", "Yojan", "Valentina", "Ximena", 
-                  "Veronica", "Valentina", "Yojan", "Ximena", 
-                  "Veronica", "Yojan", "Valentina", "Ximena")
-
-luz <- data.frame(y_luz, marca_luz,operador_luz)
-
-modelo_luz <- aov(y_luz~marca_luz+operador_luz, data = luz)
-summary(modelo_luz)
-
-#Normalidad
-qqnorm(residuals(modelo_luz))
-qqline(residuals(modelo_luz))
-
-shapiro.test(residuals(modelo_luz))
-
-# Homogeneidad
-bartlett.test(y_luz~marca_luz)
-plot(fitted(modelo_luz),residuals(modelo_luz))
-abline(h=0)
-
-# Independencia
-#Prueba de independencia Durbin Watson
-library(car)
-durbinWatsonTest(modelo_luz)
-
-#Gráfico de residuales vs orden de recolección
-plot(residuals(modelo_luz), pch =16, ylab="Residuales", xlab="Orden",
-     main="Gráfico de Orden vs Residuales")
-abline(h=0)
-
-library(asbio)
-tukey.add.test(y_luz, marca_luz, operador_luz)
-
-#Modelo interacción 
-mod <- aov(y_luz ~ marca_luz*operador_luz, data = luz)
-summary(mod)
 
 #### -------------------- Descriptivos para voltaje --------------------------------------
 # Por operador:
@@ -259,6 +169,99 @@ ggplot(voltaje_ord,aes(marca_ord,orden_vol, fill = marca_ord))+geom_boxplot()+fa
   labs(fill = "Marca", x = "Marca", y = "Voltaje")
 
 
+modelo <- aov(y_vol ~ marca + operador, data = voltaje)
+#summary(modelo)
+
+modelo_ord <- aov(orden_vol~marca_ord+operador_ord, data = voltaje_ord)
+summary(modelo_ord)
+
+#Homogeneidad
+bartlett.test(orden_vol~marca_ord)
+bartlett.test(orden_vol~operador_ord)
+
+
+#Prueba de independencia
+library(car)
+#durbinWatsonTest(modelo)
+durbinWatsonTest(modelo_ord)
+
+#Gráfico de residuales vs orden de recolección
+plot(residuals(modelo_ord), pch =16, ylab="Residuales", xlab="Orden",
+     main="Gráfico de Orden vs Residuales")
+abline(h=0)
+
+qqnorm(residuals(modelo_ord))
+qqline(residuals(modelo_ord))
+
+shapiro.test(residuals(modelo_ord))
+
+library(asbio)
+tukey.add.test(orden_vol, marca_ord, operador_ord)
+
+#Modelo de un factor
+mol_marca <- aov(orden_vol~marca_ord)
+summary(mol_marca)
+
+#Tranformación 
+modelo_trans <- aov(log(orden_vol)~marca_ord+operador_ord, data = voltaje_ord)
+summary(modelo_trans)
+
+#Homogeneidad
+#Homogeneidad
+bartlett.test(log(orden_vol)~marca_ord)
+bartlett.test(log(orden_vol)~operador_ord)
+
+#Prueba de independencia
+
+#durbinWatsonTest(modelo)
+durbinWatsonTest(modelo_trans)
+
+#Gráfico de residuales vs orden de recolección
+plot(residuals(modelo_trans), pch =16, ylab="Residuales", xlab="Orden",
+     main="Gráfico de Orden vs Residuales")
+abline(h=0)
+
+qqnorm(residuals(modelo_trans))
+qqline(residuals(modelo_trans))
+
+shapiro.test(residuals(modelo_trans))
+
+ggplot(voltaje_ord, aes(orden_vol)) +
+  geom_density(aes(fill=factor(operador_ord)), alpha=0.4)+
+  scale_fill_manual(values = c("blue", "red","green", "yellow")) +
+  labs(x = "Tiempo de reacción", y = "Densidad", fill = "Bloque")+
+  theme_bw()+scale_x_continuous(limits=c(0,1))
+
+ggplot(voltaje_ord, aes(orden_vol)) +
+  geom_density(aes(fill=factor(marca_ord)), alpha=0.4)+
+  scale_fill_manual(values = c("blue", "red","green", "yellow", "magenta")) +
+  labs(x = "Tiempo de reacción", y = "Densidad", fill = "Bloque")+
+  theme_bw()+scale_x_continuous(limits=c(0,1))
+
+
+
+############################### Luminosidad ########################
+######### Luz #########
+y_luz <- c(40, 10, 54, 96,
+           28, 31, 44, 33,
+           6, 6, 7, 14,
+           41, 44, 51, 7,
+           26, 31, 45, 11)
+
+marca_luz <-c("Duracell", "Tronex", "Futura","Futura",
+              "Varta","Varta", "Duracell", "Panasonic",
+              "Tronex", "Tronex", "Futura", "Futura",
+              "Varta", "Panasonic", "Panasonic", "Tronex",
+              "Panasonic", "Duracell", "Duracell", "Varta")
+
+operador_luz <- c("Ximena", "Valentina", "Veronica", "Yojan",
+                  "Valentina", "Yojan", "Veronica", "Ximena",
+                  "Veronica", "Yojan", "Valentina", "Ximena", 
+                  "Veronica", "Valentina", "Yojan", "Ximena", 
+                  "Veronica", "Yojan", "Valentina", "Ximena")
+
+luz <- data.frame(y_luz, marca_luz,operador_luz)
+
 ##### ---------------------------- Descriptivos luminosidad:
 # Por operador:
 by(luz$y_luz, luz$operador_luz, summary)
@@ -304,3 +307,42 @@ ggplot(luz,aes(operador_luz,y_luz, fill = operador_luz))+geom_boxplot()+facet_wr
 ggplot(luz,aes(marca_luz,y_luz, fill = marca_luz))+geom_boxplot()+facet_wrap(~operador_luz)+
   stat_summary(aes(y=y_luz, x=marca_luz),fun=mean, geom="point", shape=20,size=4, color="yellow", position = position_dodge(0.75))+
   labs(fill = "Marca", x = "Marca", y = "Voltaje")
+
+
+
+
+
+
+modelo_luz <- aov(y_luz~marca_luz+operador_luz, data = luz)
+summary(modelo_luz)
+
+#Normalidad
+qqnorm(residuals(modelo_luz))
+qqline(residuals(modelo_luz))
+
+shapiro.test(residuals(modelo_luz))
+
+# Homogeneidad
+bartlett.test(y_luz~marca_luz)
+plot(fitted(modelo_luz),residuals(modelo_luz))
+abline(h=0)
+
+# Independencia
+#Prueba de independencia Durbin Watson
+library(car)
+durbinWatsonTest(modelo_luz)
+
+#Gráfico de residuales vs orden de recolección
+plot(residuals(modelo_luz), pch =16, ylab="Residuales", xlab="Orden",
+     main="Gráfico de Orden vs Residuales")
+abline(h=0)
+
+library(asbio)
+tukey.add.test(y_luz, marca_luz, operador_luz)
+
+#Modelo interacción 
+mod <- aov(y_luz ~ marca_luz*operador_luz, data = luz)
+summary(mod)
+
+
+
